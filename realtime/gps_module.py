1d2aa268c306005453dd39410c1a22944478b8b7
brrandon13 <brrandon13@gmail.com>
2022-04-11 11:59:35 +0900

    2022.04.11

--- realtime/gps_module.py
+++ realtime/gps_module.py
@@ -11,21 +11,22 @@ class GPS:
             self.gps = 0
     
     def readMsg(self):
-        msg = self.gps.readline()
-        decoded_msg = msg.decode("utf-8")
-        data = decoded_msg.split(",")
-        if data[0] == '$GPRMC':
-            utc = data[1]
-            status = data[2]
-            if status == 'A':
-                
-                lat = data[3]
-                latDir = data[4]
-                lon = data[5]
-                lonDir = data[6]
-                kmh = data[7] * 1.852
+        while True:
+            msg = self.gps.readline()
+            decoded_msg = msg.decode("utf-8")
+            data = decoded_msg.split(",")
+            if data[0] == '$GPRMC':
+                utc = data[1]
+                status = data[2]
+                if status == 'A':
+                    
+                    lat = data[3]
+                    latDir = data[4]
+                    lon = data[5]
+                    lonDir = data[6]
+                    kmh = data[7] * 1.852
 
-                return [lat, latDir, lon, lonDir, kmh]
+                    return [lat, latDir, lon, lonDir, kmh]
         return []
 
 
